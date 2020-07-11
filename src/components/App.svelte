81608b77024e95ae10c4b797b588e94a5e138dc1
Haiturtle <Haiturtle@turtlecoin.io>
2020-07-11 18:12:53 -0400

    ADD: navbar with ice cream mascot

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,1 +1,52 @@
-<h1>What now?</h1>
+<script>
+    import { Navbar, NavbarBrand } from 'sveltestrap';
+
+    let src = "/cream.png";
+</script>
+
+<style>
+    @media (max-width: 575px) {
+        .responsive {
+            width: 100%;
+            max-width: 1.75em;
+            height: auto;
+        }
+    }
+    @media (min-width: 576px) and (max-width: 767px) {
+        .responsive {
+            width: 100%;
+            max-width: 2.25em;
+            height: auto;
+        }
+    }
+    @media (min-width: 768px) and (max-width: 991px) {
+        .responsive {
+            width: 100%;
+            max-width: 2.5em;
+            height: auto;
+        }
+     }
+    @media (min-width: 992px) and (max-width: 1199px) {
+        .responsive {
+            width: 100%;
+            max-width: 2.75em;
+            height: auto;
+        }
+     }
+    @media (min-width: 1200px) {
+        .responsive {
+            width: 100%;
+            max-width: 3em;
+            height: auto;
+        }
+     }
+</style>
+    
+<Navbar
+    color="primary"
+    dark={true}
+    light={false}
+    class="mb-2"
+>
+    <NavbarBrand href="/"><img class="responsive" {src}>Sally</NavbarBrand>
+</Navbar>
