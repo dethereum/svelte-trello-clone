d024c3850da61916efa312aa3d3cdd91af2bc7a5
Haiturtle <Haiturtle@turtlecoin.io>
2020-07-13 02:03:28 -0400

    FIX: make font actually responsive + align text middle for brand.

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <style>
-    .responsive {
+    .img-mw {
         max-width: 2rem;
     }
 </style>
@@ -13,7 +13,7 @@
 <Navbar color="primary" light class="mb-2 font-responsive font-weight-bold sticky-top">
     <div class="container">
         <NavbarBrand href="#">
-            <img class="responsive d-inline-block align-top small" {src} alt="Sally Brand">
+            <img class="img-mw d-inline-block align-middle" {src} alt="Sally Brand">
             Sally
         </NavbarBrand>
     </div>
