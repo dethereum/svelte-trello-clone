7dbfcdf59cef43e671c5cf6336ffe8110218f770
Haiturtle <Haiturtle@turtlecoin.io>
2020-07-12 21:37:45 -0400

    IMPROVE: remove normalize as bootstrap has had reboot.css since 2017 lul.

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -10,7 +10,6 @@ const templateContent = ({htmlWebpackPlugin}) => `
     <meta charset="UTF-8">
     <title>${htmlWebpackPlugin.options.title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   </head>
   <body>
