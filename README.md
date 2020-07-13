a39ac4da126ab3d038fac8d05fd653f4d41df0d9
Haiturtle <Haiturtle@turtlecoin.io>
2020-07-13 12:13:33 -0400

    ADD: https for mock production server.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 Last Updated: 07/12/20
 
 An application that has the basic functionality of the popular [Trello](https://trello.com/) webapp
+
+Run this command at root to generate local SSL certificates for the mock production server.
+
+> openssl req -x509 -newkey rsa:2048 -nodes -sha256 -keyout privateKey.key -out certificate.crt
--- tools/prodServer.js
+++ tools/prodServer.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+
 const express = require('express');
 const webpack = require('webpack');
 
@@ -5,6 +9,16 @@ const config = require('../webpack.config.prod.js');
 
 const { stats } = config
 
-module.exports = () => express()
-    .use(require('webpack-dev-middleware')(webpack(config), { stats }))
-    .use(express.static('dist'));
+const OPTIONS = {
+    key: fs.readFileSync(path.resolve(__dirname, '../privateKey.key')),
+    cert: fs.readFileSync(path.resolve(__dirname, '../certificate.crt')),
+};
+
+module.exports = () => {
+    const app = express();
+    
+    app.use(require('webpack-dev-middleware')(webpack(config), { stats }))
+    app.use(express.static('dist'));
+
+    return https.createServer(OPTIONS, app);
+}
