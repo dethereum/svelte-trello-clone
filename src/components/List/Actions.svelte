9bad6ce138fb63b739b873c7849a585609718c1f
Haiturtle <Haiturtle@turtlecoin.io>
2020-07-17 22:15:14 -0400

    ADD: basic implementation of actions modal

---
+++ src/components/List/Actions.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { Modal, ModalHeader, ModalBody, Col, Container, Row } from "sveltestrap";
+
+    let contentClassName = 'vh-100 my-3'
+
+    export let toggle = undefined;
+    export let isOpen = false
+
+    function onClick(){
+        console.log("CLICKED A BUTTON!")
+        toggle()
+    }
+</script>
+
+<Modal {isOpen} centered scrollable contentClassName={contentClassName} >
+    <ModalHeader {toggle} >
+        <h5 class="text-center font-weight-bold">Actions</h5>
+    </ModalHeader>
+    <ModalBody>
+        <Container fluid>
+            <Row class="justify-content-center">
+              <Col xs="12" >
+                <div class="font-weight-normal" on:click={onClick}>
+                    Add a Card...
+                </div>
+              </Col>
+            </Row>
+          </Container>
+    </ModalBody>
+</Modal>
--- src/components/List/Header.svelte
+++ src/components/List/Header.svelte
@@ -1,10 +1,51 @@
 <script>
     import { CardHeader } from "sveltestrap";
+
+    import Actions from "./Actions.svelte";
+
+    let hovering;
+    let isOpen;
+
+	function enter() {
+		hovering = true;
+	}
+
+	function leave() {
+		hovering = false;
+    }
+    
+    function toggle() {
+        hovering = false;
+        isOpen = !isOpen
+    }
+
+    const path = {
+        'fill-rule': "evenodd",
+        'd': "M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+    }
+
+    const svg = {
+        'width': "1em",
+        'height': "1em",
+        'viewBox': "0 0 16 16",
+        'class': "bi bi-list",
+        'fill': "currentColor",
+        'xmlns': "http://www.w3.org/2000/svg"
+    }
 </script>
+
+<style>
+    .active {
+		background-color: #000000;
+	}
+</style>
 
 <CardHeader class="font-weight-bold d-flex align-items-center justify-content-between">
 Done
-    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+<div class:active={hovering} on:mouseenter={enter} on:mouseleave={leave} on:click={toggle}>
+    <svg {...svg}> 
+        <path {...path}/>
     </svg>
+    <Actions {toggle} {isOpen}/>
+</div>
 </CardHeader>
