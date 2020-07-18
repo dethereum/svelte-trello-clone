f60f11febc94344a5de7904a061f55fd7817b92b
Haiturtle <Haiturtle@turtlecoin.io>
2020-07-18 08:40:44 -0400

    FIX: include styles to whitelist for actions for purgecss.

--- postcss.config.js
+++ postcss.config.js
@@ -5,12 +5,47 @@ const whitelist = [
   // CORE
   'body',
   'a',
-  // BOOTSTRAP DEFINED
-  'navbar-brand',
-  'navbar',
-  'bg-primary',
-  'navbar-dark',
+  // LAYOUT
+  'container-fluid',
+  'row',
+  'col-10',
+  'col-12',
+  'col-sm-8',
+  'col-md-4',
+  'col-lg-3',
+  'col-xl-2',
+  // FLEX
+  'd-flex',
+  'align-items-center',
+  'justify-content-between',
+  'justify-content-center',
+  'justify-content-md-start',
+  // SPACING
   'mb-2',
+  'mt-3',
+  'vh-100',
+  'my-3',
+  // NAV
+  'navbar',
+  'navbar-brand',
+  'navbar-dark',
+  // CARD
+  'card',
+  'card-header',
+  'card-body',
+  // MODAL
+  'modal',
+  'show',
+  'modal-dialog',
+  'modal-dialog-centered',
+  'modal-dialog-scrollable',
+  'modal-backdrop',
+  'modal-content',
+  'modal-header',
+  'modal-title',
+  'modal-body',
+  // GLOBAL
+  'bg-primary',
 ]
 
 const options = {
