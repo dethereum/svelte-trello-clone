d7badbd8dceae0390d999975a719f6882d95e101
Haiturtle <Haiturtle@turtlecoin.io>
2020-07-19 17:08:23 -0400

    ADD: virtual list esque feature to list item.

--- src/components/List/List.svelte
+++ src/components/List/List.svelte
@@ -7,6 +7,51 @@
       'Dapibus ac facilisis in vicarius dracyarus harius',
       'Morbi leo risus',
       'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
+      'Dapibus ac facilisis in vicarius dracyarus harius',
+      'Morbi leo risus',
+      'Porta ac consectetur ac',
     ]
 
 </script>
@@ -15,15 +60,38 @@
   button {
     @include font-size(0.9rem);
   }
+
+  .card-body {
+    @include media-breakpoint-between(xs, md) {
+      padding: 0.25rem;
+    }
+  }
+
+  .list-group {
+    overflow-y: auto;
+    max-height: 75vh;
+    @include media-breakpoint-between(xs, md) {
+      height: 30vh;
+    }
+    @include media-breakpoint-up(md) {
+      overflow-x: auto;
+      scrollbar-width: none;
+      -ms-overflow-style: none;
+
+      &::-webkit-scrollbar {
+        display: none;
+      }
+    }
+  }
 </style>
 
 <Card class="mt-3">
     <Header />
-    <Card body>
+    <div class="card card-body">
         <div class="list-group list-group-flush text-left">
           {#each items as item}
             <button type="button" class="list-group-item list-group-item-action">{item}</button>
           {/each}
         </div>
-    </Card>
+    </div>
 </Card>
